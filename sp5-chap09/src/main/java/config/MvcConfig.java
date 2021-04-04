ac4322b7d003843a812629fc6e80c278a79098c3
Soonho Hwang <tnsgh211@gmail.com>
2021-04-05 00:01:53 +0900

    chap09 update

--- sp5-chap09/src/main/java/config/MvcConfig.java
+++ sp5-chap09/src/main/java/config/MvcConfig.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc //스프링 mvc 설정 활성화
 public class MvcConfig implements WebMvcConfigurer {
 
-  @Override
+  @Override //DispatcherServlet의 매핑 경로를 '/'로 주었을때 정적파일을 처리하기위하 설정 추가
   public void configureDefaultServletHandling(
       DefaultServletHandlerConfigurer configurer
   ) {
